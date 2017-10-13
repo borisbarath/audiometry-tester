890f0e8310a2d8d2975d74060adcdc051862bf45
Boris Barath <boris.barath16@imperial.ac.uk>
2017-10-13 13:10:43 +0100

    Implemented getting parameters for the calculations from .txt files

--- src/Hearingapp/Utils.java
+++ src/Hearingapp/Utils.java
@@ -1,5 +1,6 @@
 package Hearingapp;
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -7,7 +8,16 @@ public class Utils {
 
   public static void readData(List<Double> array, String path) {
     //TODO: Find a way to read .txt of space-separated doubles to List
+    File file = new File(path);
+    try {
+      Scanner scanner = new Scanner(file);
+      while (scanner.hasNextDouble())
+        array.add(scanner.nextDouble());
+
+    } catch(FileNotFoundException e) {
+      e.printStackTrace();
     }
+  }
 
   public void printvals(List<Double> l){
     for(int i = 0; i < 19; i++){
