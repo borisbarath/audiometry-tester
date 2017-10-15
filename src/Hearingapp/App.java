3aa91017cdd69fcd67884bcace88eb3b0faa71bb
Boris Barath <boris.barath16@imperial.ac.uk>
2017-10-15 23:24:47 +0100

    Final GUI for version 1.0

--- src/Hearingapp/App.java
+++ src/Hearingapp/App.java
@@ -9,92 +9,108 @@ import java.util.List;
 
 public class App {
 
-//Main frame and panel which contains the result
-private JFrame frame = new JFrame();
-private JPanel centerPanel = new JPanel();
+	//Main frame and panel which contains the result
+	private JFrame frame = new JFrame();
+	private JPanel centerPanel = new JPanel(new GridBagLayout());
+	private GridBagConstraints c = new GridBagConstraints();
 
-//User-input lists for Fowler's algorithm
-private Double[] loss500;
-private Double[] loss1k;
-private Double[] loss2k;
-private Double[] loss4k;
+	//User-input lists for Fowler's algorithm
+	private Double[] loss500;
+	private Double[] loss1k;
+	private Double[] loss2k;
+	private Double[] loss4k;
 
-private JLabel space = new JLabel("     ");
+	//Combo boxes for the left ear
+	private JComboBox<Double> left500;
+	private JComboBox<Double> left1k;
+	private JComboBox<Double> left2k;
+	private JComboBox<Double> left4k;
 
-private JLabel ll500 = new JLabel("Left 500Hz:");
-private JLabel ll1k = new JLabel("Left 1000Hz:");
-private JLabel ll2k = new JLabel("Left 2000Hz:");
-private JLabel ll4k = new JLabel("Left 4000Hz:");
-private JLabel rl500 = new JLabel("Left 500Hz:");
-private JLabel rl1k = new JLabel("Left 1000Hz:");
-private JLabel rl2k = new JLabel("Left 2000Hz:");
-private JLabel rl4k = new JLabel("Left 4000Hz:");
+	//Combo boxes for the right ear
+	private JComboBox<Double> right500;
+	private JComboBox<Double> right1k;
+	private JComboBox<Double> right2k;
+	private JComboBox<Double> right4k;
 
-//Combo boxes for the left ear
-private JComboBox<Double> left500;
-private JComboBox<Double> left1k;
-private JComboBox<Double> left2k;
-private JComboBox<Double> left4k;
+	public void run() {
+		//Sets up and populates the application window
+		frame.getContentPane().add(BorderLayout.CENTER, centerPanel);
+		frame.setTitle("Fowler Hearing Loss");
 
-//Combo boxes for the right ear
-private JComboBox<Double> right500;
-private JComboBox<Double> right1k;
-private JComboBox<Double> right2k;
-private JComboBox<Double> right4k;
+		loss500 = readData("500.txt");
+		loss1k = readData("1000.txt");
+		loss2k = readData("2000.txt");
+		loss4k = readData("4000.txt");
 
-public void run() {
-	//Sets up and populates the application window
-	frame.getContentPane().add(BorderLayout.CENTER, centerPanel);
-	frame.setTitle("Fowler Hearing Loss");
+		setupInterface();
 
-	GridLayout grid = new GridLayout(4,5, 0,80);
-	centerPanel.setLayout(grid);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400, 300);
+		frame.setVisible(true);
+	}
 
-	loss500 = readData("500.txt");
-	loss1k = readData("1000.txt");
-	loss2k = readData("2000.txt");
-	loss4k = readData("4000.txt");
+	private void setupInterface() {
 
-	setupButtons();
-	setupInterface();
+		//Lists for easier generation of items
+		JComboBox[] boxes = {left500, left1k, left2k, left4k, right500, right1k, right2k, right4k};
+		int[] yCoords = {1, 2, 3, 4};
+		Double[][] lists = {loss500, loss1k, loss2k, loss4k};
+		String[] labels = {" 500", "1000", "2000", "4000"};
 
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.setSize(600, 300);
-	frame.setVisible(true);
-}
+		//Combo box setup
+		for(int i = 0; i < 8; i++){
+			boxes[i] = new JComboBox<>(lists[i%4]);
+			if(i < 4){
+				c.gridx = 1;
+			} else {
+				c.gridx = 3;
+			}
+			c.gridy = yCoords[i%4];
+			centerPanel.add(boxes[i], c);
+		}
 
-private void setupButtons() {
+		//Labels for ears
+		JLabel left = new JLabel("Left Ear");
+		c.gridx = 1;
+		c.gridy = 0;
+		centerPanel.add(left, c);
+		JLabel right = new JLabel("Right Ear");
+		c.gridx = 3;
+		c.gridy = 0;
+		centerPanel.add(right, c);
 
-	left500 = new JComboBox<>(loss500);
-	left1k = new JComboBox<>(loss1k);
-	left2k = new JComboBox<>(loss2k);
-	left4k = new JComboBox<>(loss4k);
+		//Set up labels
+		for(int i = 0; i < 8; i++){
+			JLabel label = new JLabel(labels[i%4]);
+			if(i < 4){
+				c.gridx = 0;
+			} else {
+				c.gridx = 4;
+			}
+			c.gridy = yCoords[i%4];
+			centerPanel.add(label, c);
+		}
 
-	right500 = new JComboBox<>(loss500);
-	right1k = new JComboBox<>(loss1k);
-	right2k = new JComboBox<>(loss2k);
-	right4k = new JComboBox<>(loss4k);
+		//Setup spacer between left and right combo boxes
+		for(int i = 0; i < 4; i++){
+			JLabel spacer = new JLabel("      ");
+			c.gridx = 2;
+			c.gridy = yCoords[i];
+			centerPanel.add(spacer, c);
+		}
 
-}
+		//OK button to submit values
+		JButton ok = new JButton("OK");
+		c.gridx = 2;
+		c.gridy = 5;
+		centerPanel.add(ok, c);
 
-private void setupInterface() {
-
-	centerPanel.add(ll500);
-	centerPanel.add(left500);
-	centerPanel.add(rl500);
-	centerPanel.add(right500);
-	centerPanel.add(ll1k);
-	centerPanel.add(left1k);
-	centerPanel.add(rl1k);
-	centerPanel.add(right1k);
-	centerPanel.add(ll2k);
-	centerPanel.add(left2k);
-	centerPanel.add(rl2k);
-	centerPanel.add(right2k);
-	centerPanel.add(ll4k);
-	centerPanel.add(left4k);
-	centerPanel.add(rl4k);
-	centerPanel.add(right4k);
-
-}
+		//Overall result text field
+		JTextField res = new JTextField("Result");
+		c.gridx = 1;
+		c.gridy = 6;
+		c.gridwidth = 3;
+		c.fill = GridBagConstraints.HORIZONTAL;
+		centerPanel.add(res, c);
+	}
 }
